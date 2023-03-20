e952f899738f1755e751bd2c202a1cecad9076bb
Ateox <126567320+Ateox@users.noreply.github.com>
2023-03-20 21:14:35 +0100

    Add files via upload

---
+++ Forms/FizzBuzzForm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FizzBuzzWeb.Forms
+{
+    public class FizzBuzzForm
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public int? Number { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using FizzBuzzWeb.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public FizzBuzzForm FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+            
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+        public string Information;
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid){
+                return Page();
+            }
+            if (FizzBuzz.Number%3 == 0 && FizzBuzz.Number%5 == 0)
+            {
+                Information = "FizzBuzz";
+            }else if (FizzBuzz.Number%5 == 0)
+            {
+                Information = "Buzz";
+            }else if (FizzBuzz.Number%3 == 0)
+            {
+                Information = "Fizz";
+            }else
+            {
+                Information = "Liczba "+FizzBuzz.Number+" nie spełnia kryteriów FizzBuzz";
+            }
+            return Page();
+        }
+    }
+}
